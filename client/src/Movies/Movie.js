3f03f15c6a984a5cf44c110aff57cf58f4ba1b5f
Hutch Crowley <tothecoda@gmail.com>
2019-11-06 03:12:52 -0600

    Update - Met MVP, Stable build!!!

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,22 +2,21 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({})
+  const [movie, setMovie] = useState(null)
 
   useEffect(() => {
-    const id = 1
+    const id = props.match.params.id
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
-
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then((response) => {
         setMovie(response.data)
       })
       .catch((error) => {
-        console.error(error)
+        console.error('server error', error)
       })
-  }, [])
+  }, [props.match.params.id])
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React from 'react'
 
-const MovieCard = props => {
-  return;
-};
-
-export default MovieCard;
+const MovieCard = (props) => {
+  return
+}
+export default MovieCard
