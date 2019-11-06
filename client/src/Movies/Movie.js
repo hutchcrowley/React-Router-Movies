d335e6d6a15c9f87e80968ff4ccf0113d7877b1a
Hutch Crowley <tothecoda@gmail.com>
2019-11-05 21:07:30 -0600

    init commit

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,25 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
+  const [movie, setMovie] = useState({})
+
   useEffect(() => {
-    const id = 1;
+    const id = 1
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then((response) => {
+        setMovie(response.data)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }, [])
 
-  },[]);
-  
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
@@ -27,10 +26,10 @@ const Movie = (props) => {
   // }
 
   if (!movie) {
-    return <div>Loading movie information...</div>;
+    return <div>Loading movie information...</div>
   }
 
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars } = movie
   return (
     <div className="save-wrapper">
       <div className="movie-card">
@@ -43,7 +42,7 @@ const Movie = (props) => {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars.map((star) => (
           <div key={star} className="movie-star">
             {star}
           </div>
@@ -51,7 +50,7 @@ const Movie = (props) => {
       </div>
       <div className="save-button">Save</div>
     </div>
-  );
+  )
 }
 
-export default Movie;
+export default Movie
